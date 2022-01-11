60e2db1eb3b6a8353da20d868557e9d5a5c0a49f
ayndri <dewinurayundari.dna@gmail.com>
2022-01-11 16:14:35 +0700

    database

---
+++ database/migrations/2022_01_11_160021_create_absens_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAbsensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('absens', function (Blueprint $table) {
+            $table->bigIncrements('idAbsen');
+            $table->integer('idUser')->nullable();
+            $table->dateTime('tglAbsen')->nullable();
+            $table->integer('jmlAbsen')->nullable();
+            $table->string('perihalAbsen', 40)->nullable();
+            $table->text('isiAbsen')->nullable();
+            $table->integer('statusAbsen')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('absens');
+    }
+}
