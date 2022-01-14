e35d2c0433aaa44f8ad9f192341a016f93623d6c
nopalm <nopalmdev@gmail.com>
2022-01-14 16:34:56 +0700

    renewal ongoing, fix seed

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -16,22 +16,27 @@ class RoleSeeder extends Seeder
     public function run()
     {
         Role::firstOrCreate([
+            'id' => '1',
             'name' => '1', //superadmin
         ]);
 
         Role::firstOrCreate([
+            'id' => '2',
             'name' => '2',  //admin
         ]);
 
         Role::firstOrCreate([
+            'id' => '3',
             'name' => '3',  //team
         ]);
 
         Role::firstOrCreate([
+            'id' => '4',
             'name' => '4',  //client
         ]);
 
         Role::firstOrCreate([
+            'id' => '5',
             'name' => '5',  //client
         ]);
     }
